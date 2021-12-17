cd17877278300b9cc4c564aced0e1e4220bbf29a
bmalinowsky <bmalinowsky@github.com>
2021-12-17 21:21:48 +0100

    Add testnetwork build

---
+++ build-testnetwork.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+version=2.6-SNAPSHOT
+
+rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
+    ../calimero-core ../calimero-device ../calimero-server ../calimero-rxtx ../calimero-testnetwork ./repos/
+
+#custom-liberica/build-jdk.sh $1
+(cd custom-jdk && ./build-custom-jdk.sh $1)
+
+docker build --build-arg libversion=$version -f calimero-testnetwork/Dockerfile -t calimeroproject/knxtestnetwork -t calimeroproject/knxtestnetwork:$version . $1
