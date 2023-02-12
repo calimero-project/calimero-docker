6d17c03066d17bb4bddb2808690470dbaeab86a8
bmalinowsky <bmalinowsky@github.com>
2023-02-12 16:45:13 +0100

    Update docker files for modularized build and usb provider lib

--- build-images.sh
+++ build-images.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
-    ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-rxtx ./repos/
+    ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-usb ./repos/
 
 #custom-liberica/build-jdk.sh $1
 (cd custom-jdk && ./build-custom-jdk.sh $1)
