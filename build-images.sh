6138c08714b4118c07c8f88883319e1db34445c7
bmalinowsky <bmalinowsky@github.com>
2023-09-24 18:49:37 +0200

    Build for aarch64

--- build-images.sh
+++ build-images.sh
@@ -3,8 +3,8 @@
 rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
     ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-usb ./repos/
 
-#custom-liberica/build-jdk.sh $1
-(cd custom-jdk && ./build-custom-jdk.sh $1)
+custom-liberica/build-jdk.sh $1
+#(cd custom-jdk && ./build-custom-jdk.sh $1)
 
-docker build -f calimero-tools/Dockerfile -t calimeroproject/knxtools -t calimeroproject/knxtools:2.6-SNAPSHOT . $1
-docker build -f calimero-server/Dockerfile -t calimeroproject/knxserver -t calimeroproject/knxserver:2.6-SNAPSHOT . $1
+docker buildx build --platform linux/arm64 -f calimero-tools/Dockerfile -t calimeroproject/knxtools -t calimeroproject/knxtools:3.0-SNAPSHOT . $1
+docker buildx build --platform linux/arm64 -f calimero-server/Dockerfile -t calimeroproject/knxserver -t calimeroproject/knxserver:3.0-SNAPSHOT . $1
--- custom-liberica/build-jdk.sh
+++ custom-liberica/build-jdk.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker build . --build-arg LIBERICA_IMAGE_VARIANT=base -t liberica-openjdk-alpine-musl-base $1
+docker buildx build . --platform=arm64 --build-arg LIBERICA_IMAGE_VARIANT=base -t liberica-openjdk-alpine-musl-base $1
+docker buildx build . --platform=arm64 --build-arg LIBERICA_IMAGE_VARIANT=base-minimal -t liberica-openjdk-alpine-musl-base-minimal $1
