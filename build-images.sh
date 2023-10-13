88a7b1c468ef09353f888727d00965474233f72c
bmalinowsky <bmalinowsky@github.com>
2023-10-13 23:02:26 +0200

    Build for armv7

--- build-images.sh
+++ build-images.sh
@@ -6,7 +6,7 @@ rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ -
     ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-usb ./repos/
 
 #custom-liberica/build-jdk.sh $1
-(cd custom-jdk && ./build-custom-jdk.sh $1)
+#(cd custom-jdk && ./build-custom-jdk.sh $1)
 
 docker build --build-arg libversion=$version -f calimero-tools/Dockerfile -t calimeroproject/knxtools -t calimeroproject/knxtools:$version . $1
 docker build --build-arg libversion=$version -f calimero-server/Dockerfile -t calimeroproject/knxserver -t calimeroproject/knxserver:$version . $1
