b1fdcf6c4715af6444d03e47b6a62fb0d5b13170
bmalinowsky <bmalinowsky@github.com>
2023-10-30 12:47:37 +0100

    Build with musl openjdk

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 version=3.0-SNAPSHOT
+arch=armv7
 
 rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
     ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-usb ./repos/
@@ -8,5 +9,16 @@ rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ -
 #custom-liberica/build-jdk.sh $1
 #(cd custom-jdk && ./build-custom-jdk.sh $1)
 
-docker build --build-arg libversion=$version -f calimero-tools/Dockerfile -t calimeroproject/knxtools -t calimeroproject/knxtools:$version . $1
-docker build --build-arg libversion=$version -f calimero-server/Dockerfile -t calimeroproject/knxserver -t calimeroproject/knxserver:$version . $1
+#cmd=build
+cmd="buildx build --platform=linux/arm/v7"
+
+echo
+echo "Building knxtools..."
+docker $cmd --build-arg libversion=$version -f calimero-tools/Dockerfile -t calimeroproject/knxtools -t calimeroproject/knxtools:$version \
+       -t calimeroproject/knxtools:latest-$arch -t calimeroproject/knxtools:$version-$arch . $1
+echo
+echo
+echo "Building knxserver..."
+docker $cmd --build-arg libversion=$version -f calimero-server/Dockerfile -t calimeroproject/knxserver -t calimeroproject/knxserver:$version \
+       -t calimeroproject/knxserver:latest-$arch -t calimeroproject/knxserver:$version-$arch . $1
+
