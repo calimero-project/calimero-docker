00a3f4cb3a49a9a054593ead4ec713a33017c218
bmalinowsky <bmalinowsky@github.com>
2024-01-29 11:27:22 +0100

    Print version and arch which is built

--- build-testnetwork.sh
+++ build-testnetwork.sh
@@ -11,7 +11,7 @@ rsync -av --exclude=*/.git* --exclude=.gradle/ --exclude=.settings/ --exclude=.i
 cmd="buildx build --platform=linux/$arch"
 
 echo
-echo "Building testnetwork..."
+echo "Building testnetwork $version for platform $arch..."
 docker $cmd . -f calimero-testnetwork/Dockerfile --build-arg libversion=$version --build-arg arch=$arch_alt \
     -t calimeroproject/knxtestnetwork -t calimeroproject/knxtestnetwork:$version \
     -t calimeroproject/knxtestnetwork:latest-$arch -t calimeroproject/knxtestnetwork:$version-$arch $1
