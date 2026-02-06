35cf3b3ce508d5041f12ad24670825cd09889531
bmalinowsky <bmalinowsky@github.com>
2026-02-06 14:44:03 +0100

    Exclude unnecessary files from rsync

--- build-images.sh
+++ build-images.sh
@@ -3,7 +3,7 @@
 version=3.0-M1
 arch=armv7
 
-rsync -av --exclude=.gradle/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
+rsync -av --exclude=.DS_Store --exclude=.gradle/ --exclude=.settings/ --exclude=.idea/ --exclude=bin/ --exclude=test/ --exclude=target/ --exclude=build/ --exclude=out/ \
     ../calimero-core ../calimero-tools ../serial-native ../calimero-device ../calimero-server ../calimero-usb ./repos/
 
 #custom-liberica/build-jdk.sh $1
